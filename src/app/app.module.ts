fdf8f4b991ccf27acd5440dedd3a0c8f3a38f713
Lorenzo <43671680+lorenzoGithub@users.noreply.github.com>
2019-01-24 19:26:39 +0100

    Email simple

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,10 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-import { Camera, CameraOptions } from '@ionic-native/camera';
 import { EmailComposer } from '@ionic-native/email-composer';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { EmailProvider } from '../providers/email/email';
-import { ImageProvider } from '../providers/image/image';
 
 
 @NgModule({
@@ -29,11 +26,8 @@ import { ImageProvider } from '../providers/image/image';
   providers: [
     StatusBar,
     SplashScreen,
-    Camera,
     EmailComposer,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    EmailProvider,
-    ImageProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,63 +1,28 @@
-import { ImageProvider } from './../../providers/image/image';
+import { EmailComposer } from '@ionic-native/email-composer';
 import { Component } from '@angular/core';
-import { NavController, AlertController } from 'ionic-angular';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { EmailProvider } from '../../providers/email/email';
+import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  public form: FormGroup;
-  private _attachment: any;
-  constructor(public navCtrl: NavController,
-    private _ALERT: AlertController,
-    private _FORM: FormBuilder,
-    private _IMAGE: ImageProvider,
-    private _EMAIL: EmailProvider) {
-    this.form = this._FORM.group({
-      "to": ["", Validators.required],
-      "cc": ["", Validators.required],
-      "bcc": ["", Validators.required],
-      "subject": ["", Validators.required],
-      "message": ["", Validators.required]
-    });
-  }
-  //recuperar el archivo adjunto
-  retrieveAttachment(): void {
-    this._IMAGE.selectPhotograph()
-      .then((attachment: any) => {
-        //asigna la imagen a la propiedad privada, a la cual posteriormente desde el metodo sendmessage 
-        this._attachment = attachment;
-      });
-  }
-  //Muestra el mensaje
-  displayMessage(title: string, subTitle: string): void {
-    let alert: any = this._ALERT.create({
-      title: title,
-      subTitle: subTitle,
-      buttons: ['Got it']
-    });
-    alert.present();
-  }
+   asunto='';
+   cuerpo='';
+   para='';
+  constructor(public navCtrl: NavController, public emailComposer: EmailComposer) {
 
-  sendMessage(): void {
-    //para Recupera los camps del formulario
-    let to: string = this.form.controls["to"].value,
-      cc: string = this.form.controls["cc"].value,
-      bcc: string = this.form.controls["bcc"].value,
-      subject: string = this.form.controls["subject"].value,
-      message: string = this.form.controls["message"].value;
-
-      //controla si se ha seleccionado un archivo adjunto
-    if (this._attachment.length > 1) {
-      //si se ha seleccionado llama al metodo sendEmail del emailProvider
-      this._EMAIL.sendEmail(to, cc, bcc, this._attachment, subject, message);
-    }
-    else {
-      // Informe al usuario de que necesita agregar un archivo adjunto
-      this.displayMessage('Error', 'Necesita adjuntar un archivo');
-    }
+}
+send(){
+  let email={
+    to: this.para,
+    cc:[],
+    bcc: [],
+    subject: this.asunto,
+    body: this.cuerpo,
+    isHtml: false,
+    app: "Gmail"
   }
+  this.emailComposer.open(email);
+}
 }
